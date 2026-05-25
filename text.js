eba766e6926f9fb40112199823bf8b7468f94a82
leticiaeg26 <alisello0303@gmail.com>
2026-05-25 20:03:50 +0200

    Add files via upload

---
+++ text.js
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="es" id="exe-page-mplaoozy-2bz3qpj6m">
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="eXeLearning v4.0.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="license" type="text/html" href="https://creativecommons.org/licenses/by-sa/4.0/">
+<title>Guía didáctica | Cuidamos los fondos marinos.</title>
+<link rel="icon" type="image/x-icon" href="../libs/favicon.ico">
+<meta name="description" content="Creación de un libro digital sobre los animales que viven en los fondos marinos.">
+<script>document.querySelector("html").classList.add("js");</script><script src="../libs/jquery/jquery.min.js"> </script><script src="../libs/common_i18n.js"> </script><script src="../libs/common.js"> </script><script src="../libs/exe_export.js"> </script><script src="../libs/bootstrap/bootstrap.bundle.min.js"> </script><link rel="stylesheet" href="../libs/bootstrap/bootstrap.min.css">
+<script src="../idevices/text/text.js"></script><link rel="stylesheet" href="../idevices/text/text.css">
+<link rel="stylesheet" href="../content/css/base.css"><script src="../theme/style.js"> </script><link rel="stylesheet" href="../theme/style.css">
+</head>
+<body class="exe-export exe-web-site">
+<script>document.body.className+=" js"</script>
+<div class="exe-content exe-export pre-js siteNav-hidden"><a href="#page-mplaoozy-2bz3qpj6m" id="skipNav">Skip to content</a> <nav id="siteNav">
+<ul>
+<li> <a href="../index.html" class="main-node no-ch">Inicio</a>
+</li>
+<li> <a href="../html/presentacion.html" class="no-ch">Presentación</a>
+</li>
+<li> <a href="../html/licencias-y-seguridad.html" class="no-ch">Licencias y seguridad</a>
+</li>
+<li> <a href="../html/curacion-de-contenidos.html" class="daddy">Curación de contenidos</a>
+<ul class="other-section">
+<li> <a href="../html/padlet.html" class="no-ch">Padlet</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/linea-temporal.html" class="no-ch">Línea temporal</a>
+</li>
+<li> <a href="../html/tareas.html" class="daddy">Tareas</a>
+<ul class="other-section">
+<li> <a href="../html/sesiones.html" class="no-ch">Sesiones</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/instrumentos-de-evaluacion.html" class="no-ch">Instrumentos de evaluación</a>
+</li>
+<li class="active"> <a href="../html/guia-didactica.html" class="active no-ch">Guía didáctica</a>
+</li>
+<li> <a href="../html/creditos.html" class="no-ch">Créditos</a>
+</li>
+</ul>
+</nav><main id="page-mplaoozy-2bz3qpj6m" class="page"> 
+<header class="main-header">
+<div class="package-header"><p class="package-title">Cuidamos los fondos marinos.</p></div>
+<div class="page-header"><h1 class="page-title">Guía didáctica</h1></div>
+</header><div id="page-content-page-mplaoozy-2bz3qpj6m" class="page-content">
+<article id="block-1779214197405-w0ipts8mz" class="box no-header">
+<header class="box-head no-icon">
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1779214197395-362vi8jze" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1779214197395-362vi8jze&quot;}">
+<div class="exe-text-template">
+    <p><img src="../content/resources/Guia didactica.png" width="1366" height="342" alt="" /></p>
+</div>
+</div>
+</div>
+</article><article id="block-1779214963101-5sqvas0am" class="box no-header">
+<header class="box-head no-icon">
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1779214963093-abtzrby07" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1779214963093-abtzrby07&quot;}">
+<div class="exe-text-template">
+    <p><strong>Curso al que va dirigido:</strong></p>
+<p>4º de Educación Primaria.</p>
+<p><br /><strong>Objetivo:</strong></p>
+<p>Se pretende que el alumnado conozca la importancia del cuidado del medio ambiente y en especial de la flora marina y las consecuencias que puede tener el deterioro del mismo a causa de los seres humanos.</p>
+<p><br /><strong>Temporalización:</strong> 3 semanas.</p>
+<ul>
+<li>2 sesiones semanales de Conocimiento del Medio.</li>
+<li>2 sesiones de Lengua.</li>
+<li>1 sesión de Artística.</li>
+<li>2 sesiones de Matemáticas.</li>
+</ul>
+<p><br /><strong>Actividades:</strong></p>
+<ul>
+<li>Investigación.</li>
+<li>Creación de juegos de mesa sobre fondos marinos.</li>
+<li>Creación de un libro digital sobre animales que viven en el fondo del mar.</li>
+<li>Cuentacuentos a nuestro alumnado de infantil.</li>
+</ul>
+<p><br /><strong>Evaluación:</strong></p>
+<ul>
+<li>La evaluación de esta SdA será continua, global y con un carácter formativo.</li>
+<li>Para ello se hace necesaria la exposición de los criterios de evaluación, ya que se prioriza una evaluación transparente.</li>
+<li>A continuación en el siguiente enlace se podrá visualizar, la tarea a trabajar junto con su criterio asociado, tipo de agrupamiento, cuanto tiempo se necesita para realizar dicha actividad y una posible herramienta para realizar la tarea que se pide.</li>
+<li title=""><a href="guia-didactica.html" rel="author noopener" target="_blank">Asociamos criterios con actividades</a></li>
+</ul>
+</div>
+</div>
+</div>
+</article>
+</div></main><div class="nav-buttons">
+<a href="../html/instrumentos-de-evaluacion.html" title="Anterior" class="nav-button nav-button-left"><span>Anterior</span></a>
+<a href="../html/creditos.html" title="Siguiente" class="nav-button nav-button-right"><span>Siguiente</span></a>
+</div>
+<footer id="siteFooter"><div id="siteFooterContent"> <div id="packageLicense" class="cc cc-by-sa"> <p> <span class="license-label">Licencia: </span><a href="https://creativecommons.org/licenses/by-sa/4.0/" class="license">Creative Commons: Reconocimiento - compartir igual 4.0</a></p>
+</div>
+</div></footer>
+</div>
+<p id="made-with-eXe"> <a href="https://exelearning.net/" target="_blank" rel="noopener"> <span>Creado con eXeLearning <span>(nueva ventana)</span></span></a></p>
+</body>
+</html>
